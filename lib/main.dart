9e3ccda4e30341a7f4aef88abb928d1dca0d9898
Shwin Pyone Thu <6531503183@lamduan.mfu.ac.th>
2025-02-06 20:39:40 +0700

    login page shwin

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'home.dart';
+import 'login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomeScreen(),
+      home: const SignInPage2(),
     );
   }
 }
