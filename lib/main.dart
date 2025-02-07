32e8ddbbf541ff22707732ade4eb413269c0f95d
Shwin Pyone Thu <6531503183@lamduan.mfu.ac.th>
2025-02-07 12:11:03 +0700

    merge

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'home.dart';
-import 'login.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const SignInPage2(),
+      home: const HomeScreen(),
     );
   }
 }
